c2693896508f7c1c85ee335ae10cfd73c93c8357
Phetogok <pkwape33@gmail.com>
2025-07-14 14:15:33 +0200

    Add some javascript code

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ const products = [
     name: "iphone 15 pro",
     price: 999,
     category: "phones",
-    image:
-      "https://via.placeholder.com/300x200/3b82f6/white?text=iPhone+15+Pro",
+    image:"https://www.mobalife.co.za/wp-content/uploads/2024/08/Iphone-15-Pro-Max.jpg",
     description: "The latest iPhone with amazing camera and performance",
   },
   {
@@ -13,15 +12,15 @@ const products = [
     name: "MacBook Air",
     price: 1199,
     category: "laptops",
-    image: "https://via.placeholder.com/300x200/10b981/white?text=MacBook+Air",
+    image: "https://www.istore.co.za/media/catalog/product/cache/7cbfd4bf9761b066f119e95af17e67c5/m/a/macbook_air_13_in_m3_midnight_pdp_image_position_1__wwen.jpg",
     description: "Lightweight laptop perfect for work and creativity",
   },
   {
     id: 3,
     name: "AirPods Pro",
     price: 249,
-    category: "phones",
-    image: "https://via.placeholder.com/300x200/f59e0b/white?text=AirPods+Pro",
+    category: "accessories",
+    image: "https://www.digicape.co.za/image/cache/catalog/_products/2024/AirPods/AirPods_4/AirPods_4_1-1000x1000.jpg",
     description: "Wireless earbuds with noise cancellation",
   },
   {
@@ -29,15 +28,15 @@ const products = [
     name: "Samsung Galaxy S24",
     price: 899,
     category: "phones",
-    image: "https://via.placeholder.com/300x200/8b5cf6/white?text=Galaxy+S24",
+    image: "https://www.technomobi.co.za/media/catalog/product/cache/e2a07cde3e161746aa285b73c334c4ba/1/0/104073413_588bec06e9a74b43f487e76f293a522f.png",
     description: "Android phone with incredible features",
   },
   {
     id: 5,
     name: "Dell Laptop",
     price: 799,
-    category: "phones",
-    image: "https://via.placeholder.com/300x200/06b6d4/white?text=Dell+Laptop",
+    category: "laptop",
+    image: "https://tech.co.za/wp-content/uploads/2024/09/Latitude-3450d.png",
     description: "Reliable laptop for everyday computing",
   },
   {
@@ -46,7 +45,7 @@ const products = [
     price: 49,
     category: "accessories",
     image:
-      "https://via.placeholder.com/300x200/ec4899/white?text=Wireless+Mouse",
+      "https://5.imimg.com/data5/SELLER/Default/2022/10/PV/EV/WE/117485675/seenda-wireless-silent-mouse-with-usb-nano-receiver-1600-dpi-ambidextrous-pc-mac-laptop-black-500x500.jpg",
     description: "Ergonomic wireless mouse for productivity",
   },
 ];
@@ -73,13 +72,13 @@ function formatPrice(price) {
 function createProductCard(product) {
   return `
         <div class="product-card">
-            <img src="${product.image}" alt="${product.name} class="product-image">
+            <img src="${product.image}" alt="${product.name}" class="product-image">
             <div class="product-info">
                 <h3 class="product-title">${product.name}</h3>
                 <p class="product-description">${product.description}</p>
                 <div class="product-price">${formatPrice(product.price)}</div>
                 <div class="product-actions">
-                    <button class="btn btn-primary btn-small" onclick="addToCart(${product})"> Add to Cart </button>
+                    <button class="btn btn-primary btn-small" onclick="addToCart(${product.id})"> Add to Cart </button>
                     <button class="btn btn-secondary btn-small" onclick="viewProducts(${product.id})"> View Details </button>
                 </div>
             </div>
@@ -90,6 +89,21 @@ function createProductCard(product) {
 function displayProducts(productsToShow = products){
     if(productsGrid){
         const productsHTML = productsToShow.map(createProductCard).join('');
-        productsGrid.innerHTML = productsHTML 
+        productsGrid.innerHTML = productsHTML;
     }
 }
+
+function addToCart(productID){
+  alert(`Product ${productID} added to cart`);
+
+}
+
+function viewProducts(productID){
+  const product = products.find(pro => pro.id === productID);
+  alert('Product: ' + product.name + '\nPrice: ' + formatPrice(product.price) + '\nDescription: ' + product.description)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Page loading, Products')
+  displayProducts()
+})
